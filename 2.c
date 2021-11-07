214ee6a4510b3979f44030621d9a4c076bf54c8f
Kestrel-omega <jiho604@naver.com>
2021-11-08 01:30:16 +0900

    20211108_jiho.k

--- 2.c
+++ 2.c
@@ -1,6 +1,7 @@
 #include "myProject.h"
 
-int printMyName2(void) {
-    printf("2. my name is [이름입력]\n"); //자기 이름
+int printMyName2(void) 
+{
+    printf("2. my name is Jiho Kim\n"); //자기 이름
     return 1;
 }
--- myProject.h
+++ myProject.h
@@ -1,2 +1,10 @@
+#pragma once
+#ifndef __MY_PROJECT_H__
+#define __MY_PROJECT_H__
+
+#include <stdio.h>
+
 int printMyName1(void);
 int printMyName2(void);
+
+#endif
