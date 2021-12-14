bc98add861adf61c99da40b8da0bc1732e8ad933
Jongsang yoo <jamin0513@gmail.com>
2021-12-15 01:54:11 +0900

    코드 리팩토링

--- Project/main.c
+++ Project/main.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #define MENU 0
 #define LEVEL1 1
 #define LEVEL2 2
@@ -14,20 +16,24 @@
 #define START 13
 #define EXIT 14
 
-#define hurdle_0_x1 0
-#define hurdle_0_x2 0
-#define hurdle_0_y1 0
-#define hurdle_0_y2 0
-#define hurdle_1_x1 0
-#define hurdle_1_x2 0
-#define hurdle_1_y1 0
-#define hurdle_1_y2 0
-#define hurdle_2_x1 0
-#define hurdle_2_x2 0
-#define hurdle_2_y1 0
-#define hurdle_2_y2 0
+#define WIDTH 0
+#define HIGH 0
+#define DES_SENSTIVE 30
+
+const int DESTINATION_S1 [8][2]  = {{0,0},
+                                    {0,0}};
+const int DESTINATION_S2 [8][2]  = {{0,0},
+                                    {0,0}};
+const int DESTINATION_S3 [8][2]  = {{0,0},
+                                    {0,0}};
+
+const int HURDLE_0[2] = {0,}; //왼쪽 위 꼭지점 좌표 hurdle_0[0] : x, hurdle_0[1] : y
+const int HURDLE_1[2] = {0,};
+const int HURDLE_2[2] = {0,};
 
 void HW_init();
+void HW_close();
+
 void Level1(int stage);
 void Level2(int stage);
 void Level3(int stage);
@@ -39,14 +45,16 @@ void Level3_Playgame(int destinationX, int destinationY, int nextStage);
 void Ball_display1(int ballLocationX, int ballLocationY);
 void Ball_display2(int ballLocationX, int ballLocationY);
 void Ball_display3(int ballLocationX, int ballLocationY);
+
 int set_Mobility();
+int angle_x(int accel_y ,int accel_z);
+int angle_y(int accel_x, int accel_y ,int accel_z);
 
 int ballLocation[2] = {0,0};
 int status = MENU;
 int stage = STAGE1;
 int key_value = 0;
 int mobility;
-
 
 void main(){
     HW_init();
@@ -95,43 +103,43 @@ void Level1(int stage){
     int destination[2];
     switch (stage) {
         case STAGE1:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[0][0];
+            destination[1] = DESTINATION_S1[0][1];
             Level1_Playgame(destination[0],destination[1],STAGE2);
             break;
         case STAGE2:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[1][0];
+            destination[1] = DESTINATION_S1[1][1];
             Level1_Playgame(destination[0],destination[1],STAGE3);
             break;
         case STAGE3:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[2][0];
+            destination[1] = DESTINATION_S1[2][1];
             Level1_Playgame(destination[0],destination[1],STAGE4);
             break;
         case STAGE4:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[3][0];
+            destination[1] = DESTINATION_S1[3][1];
             Level1_Playgame(destination[0],destination[1],STAGE5);
             break;
         case STAGE5:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[4][0];
+            destination[1] = DESTINATION_S1[4][1];
             Level1_Playgame(destination[0],destination[1],STAGE6);
             break;
         case STAGE6:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[5][0];
+            destination[1] = DESTINATION_S1[5][1];
             Level1_Playgame(destination[0],destination[1],STAGE7);
             break;
         case STAGE7:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[6][0];
+            destination[1] = DESTINATION_S1[6][1];
             Level1_Playgame(destination[0],destination[1],STAGE8);
             break;
         case STAGE8:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S1[7][1];
+            destination[1] = DESTINATION_S1[7][1];
             Level1_Playgame(destination[0],destination[1],STAGE1);
             break;
         default:
@@ -144,43 +152,43 @@ void Level2(int stage){
     int destination[2];
     switch (stage) {
         case STAGE1:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[0][0];
+            destination[1] = DESTINATION_S2[0][0];
             Level2_Playgame(destination[0],destination[1],STAGE2);
             break;
         case STAGE2:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[1][0];
+            destination[1] = DESTINATION_S2[1][1];
             Level2_Playgame(destination[0],destination[1],STAGE3);
             break;
         case STAGE3:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[2][0];
+            destination[1] = DESTINATION_S2[2][1];
             Level2_Playgame(destination[0],destination[1],STAGE4);
             break;
         case STAGE4:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[3][0];
+            destination[1] = DESTINATION_S2[3][1];
             Level2_Playgame(destination[0],destination[1],STAGE5);
             break;
         case STAGE5:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[4][0];
+            destination[1] = DESTINATION_S2[4][1];
             Level2_Playgame(destination[0],destination[1],STAGE6);
             break;
         case STAGE6:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[5][0];
+            destination[1] = DESTINATION_S2[5][1];
             Level2_Playgame(destination[0],destination[1],STAGE7);
             break;
         case STAGE7:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[6][0];
+            destination[1] = DESTINATION_S2[6][1];
             Level2_Playgame(destination[0],destination[1],STAGE8);
             break;
         case STAGE8:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S2[7][0];
+            destination[1] = DESTINATION_S2[7][1];
             Level2_Playgame(destination[0],destination[1],STAGE1);
             break;
         default:
@@ -190,46 +198,45 @@ void Level2(int stage){
 
 void Level3(int stage){
     int destination[2];
-
     switch (stage) {
         case STAGE1:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[0][0];
+            destination[1] = DESTINATION_S3[0][1];
             Level3_Playgame(destination[0],destination[1],STAGE2);
             break;
         case STAGE2:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[1][0];
+            destination[1] = DESTINATION_S3[1][1];
             Level3_Playgame(destination[0],destination[1],STAGE3);
             break;
         case STAGE3:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[2][0];
+            destination[1] = DESTINATION_S3[2][1];
             Level3_Playgame(destination[0],destination[1],STAGE4);
             break;
         case STAGE4:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[3][0];
+            destination[1] = DESTINATION_S3[3][1];
             Level3_Playgame(destination[0],destination[1],STAGE5);
             break;
         case STAGE5:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[4][0];
+            destination[1] = DESTINATION_S3[4][1];
             Level3_Playgame(destination[0],destination[1],STAGE6);
             break;
         case STAGE6:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[5][0];
+            destination[1] = DESTINATION_S3[5][1];
             Level3_Playgame(destination[0],destination[1],STAGE7);
             break;
         case STAGE7:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[6][0];
+            destination[1] = DESTINATION_S3[6][1];
             Level3_Playgame(destination[0],destination[1],STAGE8);
             break;
         case STAGE8:
-            destination[0] = ;
-            destination[1] = ;
+            destination[0] = DESTINATION_S3[7][0];
+            destination[1] = DESTINATION_S3[7][1];
             Level3_Playgame(destination[0],destination[1],STAGE1);
             break;
         default:
@@ -241,7 +248,6 @@ void Level3(int stage){
 void Level1_Playgame(int destination_x, int destination_y,int nextStage){
     int destination[2] = {destination_x,destination_y};
     int ballSpeed[2];
-    int hurdle_0[4]  = {hurdle_0_x1, hurdle_0_x2, hurdle_0_y1, hurdle_0_y2};
 
     int des_Flag[2] = {0,0};
     int deadLine_Flag0[2];
@@ -254,13 +260,12 @@ void Level1_Playgame(int destination_x, int destination_y,int nextStage){
         ballLocation[0] = ballLocation[0] + ballSpeed[0];
         ballLocation[1] = ballLocation[1] + ballSpeed[1];
         
-
         //장애물 검출 알고리즘 입력
-        if (( hurdle_0[0] -10 < ballLocation[0] ) && ( ballLocation[0] < hurdle_0[1] +10 )) deadLine_Flag0[0] = 1;
-        if (( hurdle_0[2] -10 < ballLocation[1] ) && ( ballLocation[1] < hurdle_0[3] +10 )) deadLine_Flag0[1] = 1;
+        if (( HURDLE_0[0] - WIDTH < ballLocation[0] ) && ( ballLocation[0] < HURDLE_0[0] + WIDTH )) deadLine_Flag0[0] = 1;
+        if (( HURDLE_0[1] - HIGH  < ballLocation[1] ) && ( ballLocation[1] < HURDLE_0[1] + HIGH )) deadLine_Flag0[1] = 1;
 
-        if (( destination[0] -10 < ballLocation[0] ) && ( ballLocation[0] < destination[0] +10 )) des_Flag[0] = 1;
-        if (( destination[1] -10 < ballLocation[1] ) && ( ballLocation[1] < destination[1] +10 )) des_Flag[1] = 1;
+        if (( destination[0] - DES_SENSTIVE < ballLocation[1] ) && ( ballLocation[1] < destination[1] + DES_SENSTIVE )) des_Flag[0] = 1;
+        if (( destination[0] - DES_SENSTIVE < ballLocation[1] ) && ( ballLocation[1] < destination[1] + DES_SENSTIVE )) des_Flag[1] = 1;
 
         if (des_Flag[0] && des_Flag[1]) {
             result = 1;
@@ -270,7 +275,7 @@ void Level1_Playgame(int destination_x, int destination_y,int nextStage){
             result = 0;
             break;
         }
-        Ball_display1(ballLocation[0], ballLocation[1], hurdle_0[4]);
+        Ball_display1(ballLocation[0], ballLocation[1]);
     }
     if(nextStage == STAGE1) {
         if (result == 1) {
@@ -289,9 +294,6 @@ void Level2_Playgame(int destination_x, int destination_y, int nextStage){
     int destination[2] = {destination_x,destination_y};
     int ballSpeed[2];
 
-    int hurdle_0[4]  = {hurdle_0_x1, hurdle_0_x2, hurdle_0_y1, hurdle_0_y2};
-    int hurdle_1[4]  = {hurdle_1_x1, hurdle_1_x2, hurdle_1_y1, hurdle_1_y2};
-
     int des_Flag[2] = {0,0};
     int deadLine_Flag0[2];
     int deadLine_Flag1[2];
@@ -305,27 +307,27 @@ void Level2_Playgame(int destination_x, int destination_y, int nextStage){
         ballLocation[1] = ballLocation[1] + ballSpeed[1];
         
         //장애물 검출 알고리즘 입력
-        if (( hurdle_0[0] -10 < ballLocation[0] ) && ( ballLocation[0] < hurdle_0[1] +10 )) deadLine_Flag0[0] = 1;
-        if (( hurdle_0[2] -10 < ballLocation[1] ) && ( ballLocation[1] < hurdle_0[3] +10 )) deadLine_Flag0[1] = 1;
-        if (( hurdle_1[0] -10 < ballLocation[0] ) && ( ballLocation[0] < hurdle_1[1] +10 )) deadLine_Flag1[0] = 1;
-        if (( hurdle_1[2] -10 < ballLocation[1] ) && ( ballLocation[1] < hurdle_1[3] +10 )) deadLine_Flag1[1] = 1;
+        if (( HURDLE_0[0] - WIDTH < ballLocation[0] ) && ( ballLocation[0] < HURDLE_0[0] + WIDTH )) deadLine_Flag0[0] = 1;
+        if (( HURDLE_0[1] - HIGH  < ballLocation[1] ) && ( ballLocation[1] < HURDLE_0[1] + HIGH )) deadLine_Flag0[1] = 1;
+        if (( HURDLE_1[0] - WIDTH < ballLocation[0] ) && ( ballLocation[0] < HURDLE_1[0] + WIDTH )) deadLine_Flag1[0] = 1;
+        if (( HURDLE_1[1] - HIGH  < ballLocation[1] ) && ( ballLocation[1] < HURDLE_1[1] + HIGH )) deadLine_Flag1[1] = 1;
 
-        if (( destination[0] -10 < ballLocation[0] ) && ( ballLocation[0] < destination[0] +10 )) des_Flag[0] = 1;
-        if (( destination[1] -10 < ballLocation[1] ) && ( ballLocation[1] < destination[1] +10 )) des_Flag[1] = 1;
+        if (( destination[0] - DES_SENSTIVE < ballLocation[1] ) && ( ballLocation[1] < destination[1] + DES_SENSTIVE )) des_Flag[0] = 1;
+        if (( destination[0] - DES_SENSTIVE < ballLocation[1] ) && ( ballLocation[1] < destination[1] + DES_SENSTIVE )) des_Flag[1] = 1;
 
         if (des_Flag[0] && des_Flag[1]) {
             result = 1;
             break;
         }
         if (deadLine_Flag0[0] && deadLine_Flag0[1]) {
-            result = 0;
+            result = 1;
             break;
         }
         if (deadLine_Flag1[0] && deadLine_Flag1[1]) {
-            result = 0;
+            result = 1;
             break;
         }
-        Ball_display2(ballLocation[0], ballLocation[1], hurdle_0[4], hurdle_1[4]);
+        Ball_display2(ballLocation[0], ballLocation[1]);
     }
     if(nextStage == STAGE1) {
         if (result == 1) {
@@ -344,10 +346,6 @@ void Level3_Playgame(int destination_x, int destination_y, int nextStage){
     int destination[2] = {destination_x,destination_y};
     int ballSpeed[2];
 
-    int hurdle_0[4]  = {hurdle_0_x1, hurdle_0_x2, hurdle_0_y1, hurdle_0_y2};
-    int hurdle_1[4]  = {hurdle_1_x1, hurdle_1_x2, hurdle_1_y1, hurdle_1_y2};
-    int hurdle_2[4]  = {hurdle_2_x1, hurdle_2_x2, hurdle_2_y1, hurdle_2_y2};
-
     int des_Flag[2] = {0,0};
     int deadLine_Flag0[2];
     int deadLine_Flag1[2];
@@ -361,17 +359,16 @@ void Level3_Playgame(int destination_x, int destination_y, int nextStage){
         ballLocation[0] = ballLocation[0] + ballSpeed[0];
         ballLocation[1] = ballLocation[1] + ballSpeed[1];
         
-
         //장애물 검출 알고리즘 입력
-        if (( hurdle_0[0] -10 < ballLocation[0] ) && ( ballLocation[0] < hurdle_0[1] +10 )) deadLine_Flag0[0] = 1;
-        if (( hurdle_0[2] -10 < ballLocation[1] ) && ( ballLocation[1] < hurdle_0[3] +10 )) deadLine_Flag0[1] = 1;
-        if (( hurdle_1[0] -10 < ballLocation[0] ) && ( ballLocation[0] < hurdle_1[1] +10 )) deadLine_Flag1[0] = 1;
-        if (( hurdle_1[2] -10 < ballLocation[1] ) && ( ballLocation[1] < hurdle_1[3] +10 )) deadLine_Flag1[1] = 1;
-        if (( hurdle_2[0] -10 < ballLocation[0] ) && ( ballLocation[0] < hurdle_2[1] +10 )) deadLine_Flag2[0] = 1;
-        if (( hurdle_2[2] -10 < ballLocation[1] ) && ( ballLocation[1] < hurdle_2[3] +10 )) deadLine_Flag2[1] = 1;
+        if (( HURDLE_0[0] - WIDTH < ballLocation[0] ) && ( ballLocation[0] < HURDLE_0[0] + WIDTH )) deadLine_Flag0[0] = 1;
+        if (( HURDLE_0[1] - HIGH  < ballLocation[1] ) && ( ballLocation[1] < HURDLE_0[1] + HIGH )) deadLine_Flag0[1] = 1;
+        if (( HURDLE_1[0] - WIDTH < ballLocation[0] ) && ( ballLocation[0] < HURDLE_1[0] + WIDTH )) deadLine_Flag1[0] = 1;
+        if (( HURDLE_1[1] - HIGH  < ballLocation[1] ) && ( ballLocation[1] < HURDLE_1[1] + HIGH )) deadLine_Flag1[1] = 1;
+        if (( HURDLE_2[0] - WIDTH < ballLocation[0] ) && ( ballLocation[1] < HURDLE_2[0] + WIDTH )) deadLine_Flag2[1] = 1;
+        if (( HURDLE_2[1] - HIGH  < ballLocation[1] ) && ( ballLocation[1] < HURDLE_2[1] + HIGH )) deadLine_Flag2[1] = 1;
 
-        if (( destination[0] -10 < ballLocation[0] ) && ( ballLocation[0] < destination[1] +10 )) des_Flag[0] = 1;
-        if (( destination[0] -10 < ballLocation[1] ) && ( ballLocation[1] < destination[1] +10 )) des_Flag[1] = 1;
+        if (( destination[0] - DES_SENSTIVE < ballLocation[1] ) && ( ballLocation[1] < destination[1] + DES_SENSTIVE )) des_Flag[0] = 1;
+        if (( destination[0] - DES_SENSTIVE < ballLocation[1] ) && ( ballLocation[1] < destination[1] + DES_SENSTIVE )) des_Flag[1] = 1;
 
         if (des_Flag[0] && des_Flag[1]) {
             result = 1;
@@ -434,4 +431,24 @@ int set_Mobility(){
     }
     else;
     return mobility;
+}
+
+void HW_init(){
+    init_accel();
+}
+
+void HW_close(){
+    close_accel();
+}
+
+int angle_x(){
+    int accel_y = read_accel(y);
+    int accel_z = read_accel(z);
+    return atan(accel_y / accel_z) * (180 / 3.14);
+}
+int angle_y(){
+    int accel_x = read_accel(x);
+    int accel_y = read_accel(y);
+    int accel_z = read_accel(z);
+    return atan(accel_x/ sqrt(accel_y*accel_y + accel_z*accel_z)) * (180 / 3.14);
 }
