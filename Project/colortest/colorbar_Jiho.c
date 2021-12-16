537e55f691e6c761109ac38d9493d1f37b9f9035
Kestrel-omega <jiho604@naver.com>
2021-12-16 22:32:31 +0900

    display

--- Project/colortest/colorbar_Jiho.c
+++ Project/colortest/colorbar_Jiho.c
@@ -139,68 +139,65 @@
 #include <sys/mman.h>
 #include <linux/fb.h> // for fb_var_screeninfo, FBIOGET_VSCREENINFO
 
+#define FBDEV_FILE0 "/dev/fb0"
 #define FBDEV_FILE1 "/dev/fb1"
-#define FBDEV_FILE "/dev/fb0"
+
 #define DES_size_x 500
 #define BALL_size_x 100
 #define WIDTH 51
 #define HIGH 30
 
 void Ball_DP(int x1, int y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height);
+void Ball_RM(int x1, int y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height);
 void Back_ground(int COLOR, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height);
 void hurdle_0(int X1, int Y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height);
 void hurdle_1(int X1, int Y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height);
 void DES(int X1, int Y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height);
 
-int main(int argc, char **argv)
+int fb_fd0;
+int fb_fd1;
+struct fb_var_screeninfo fbvar;
+struct fb_fix_screeninfo fbfix;
+unsigned char *fb_mapped0;
+unsigned char *fb_mapped1;
+unsigned long *ptr0;
+unsigned long *ptr1;
+
+int screen_width;
+int screen_height;
+int bits_per_pixel;
+int line_length;
+
+int mem_size;
+
+int display_init(void)
 {
     // 커서 제거
     int conFD = open("/dev/tty0", O_RDWR);
     ioctl(conFD, KDSETMODE, KD_GRAPHICS);
     close(conFD);
 
-    int screen_width;
-    int screen_height;
-    int bits_per_pixel;
-    int line_length;
-
-    int fb_fd;
-    int fb_fd1;
-    struct fb_var_screeninfo fbvar;
-    struct fb_fix_screeninfo fbfix;
-    unsigned char *fb_mapped;
-    unsigned char *fb_mapped1;
-    int mem_size_x;
-    unsigned long *ptr;
-    unsigned long *ptr1;
-    int coor_y;
-    int coor_x;
-
-    printf("===================================\n");
-    printf("Frame buffer Application - ColorBar\n");
-    printf("===================================\n");
-
-    if (access(FBDEV_FILE, F_OK))
+    if (access(FBDEV_FILE0, F_OK))
     {
-        printf("%s: access error\n", FBDEV_FILE);
+        printf("%s: access error\n", FBDEV_FILE0);
         exit(1);
     }
 
-    if ((fb_fd = open(FBDEV_FILE, O_RDWR)) < 0)
+    if ((fb_fd0 = open(FBDEV_FILE0, O_RDWR)) < 0)
     {
-        printf("%s: open error\n", FBDEV_FILE);
+        printf("%s: open error\n", FBDEV_FILE0);
         exit(1);
     }
 
-    if (ioctl(fb_fd, FBIOGET_VSCREENINFO, &fbvar))
+    if (ioctl(fb_fd0, FBIOGET_VSCREENINFO, &fbvar))
     {
-        printf("%s: ioctl error - FBIOGET_VSCREENINFO \n", FBDEV_FILE);
+        printf("%s: ioctl error - FBIOGET_VSCREENINFO \n", FBDEV_FILE0);
         exit(1);
     }
 
-    if (ioctl(fb_fd, FBIOGET_FSCREENINFO, &fbfix))
+    if (ioctl(fb_fd0, FBIOGET_FSCREENINFO, &fbfix))
     {
-        printf("%s: ioctl error - FBIOGET_FSCREENINFO \n", FBDEV_FILE);
+        printf("%s: ioctl error - FBIOGET_FSCREENINFO \n", FBDEV_FILE0);
         exit(1);
     }
 
@@ -238,44 +235,51 @@ int main(int argc, char **argv)
     printf("bits_per_pixel : %d\n", bits_per_pixel);
     printf("line_length : %d\n", line_length);
 
-    mem_size_x = screen_width * screen_height * 4;
-    fb_mapped = (unsigned char *)mmap(0, mem_size_x,
-                                      PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
-    fb_mapped1 = (unsigned char *)mmap(0, mem_size_x,
-                                       PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd1, 0);
+    mem_size = screen_width * screen_height * 4;
+    fb_mapped0 = (unsigned char *)mmap(0, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd0, 0);
+    fb_mapped1 = (unsigned char *)mmap(0, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd1, 0);
 
-    if (fb_mapped < 0 || fb_mapped1 < 0)
+    if (fb_mapped0 < 0 || fb_mapped1 < 0)
     {
         printf("mmap error!\n");
         exit(1);
     }
 
-    // Back_ground(0xFF0000, ptr1, fb_mapped1, screen_width, screen_height);
-    // Back_ground(0xFF0000, ptr, fb_mapped, screen_width, screen_height);
-  
-    // Ball_DP(462, 250, ptr, fb_mapped, screen_width, screen_height);
+    return 0;
+}
 
-    while(1)
-    {
-        Back_ground(0xFF0000, ptr1, fb_mapped1, screen_width, screen_height);
-        // Back_ground(0xFF0000, ptr, fb_mapped, screen_width, screen_height);
-        Ball_DP(462, 250, ptr, fb_mapped, screen_width, screen_height);
-        usleep(1000000);
-        Back_ground(0x00FF00, ptr, fb_mapped, screen_width, screen_height);
-        Ball_DP(462, 250, ptr, fb_mapped, screen_width, screen_height);
-        usleep(1000000);
-        Back_ground(0x0000FF, ptr, fb_mapped, screen_width, screen_height);
-        Ball_DP(462, 250, ptr, fb_mapped, screen_width, screen_height);
-        usleep(1000000);
-    }
-
-    //카메라 상에서는 혜성처럼 꼬리가 생김. 실제로는 어떻게 작동하는지 검증이 필요함.
-    munmap(fb_mapped, mem_size_x);
-    close(fb_fd);
-    munmap(fb_mapped1, mem_size_x);
+int end_display(void)
+{
+    munmap(fb_mapped0, mem_size);
+    close(fb_fd0);
+    munmap(fb_mapped1, mem_size);
     close(fb_fd1);
     return 0;
 }
+
+int main(int argc, char **argv)
+{
+    display_init();
+     
+    printf("===================================\n");
+    printf("Frame buffer Application - ColorBar\n");
+    printf("===================================\n");
+
+    Back_ground(0x000000, ptr0, fb_mapped0, screen_width, screen_height);
+    for (int i = 0; i < 1024; i++)
+    {
+        Draw_Square(i, i * 600 / 1024, 40, 40, ptr0, fb_mapped0, screen_width, screen_height);
+        usleep(10000);
+        Back_ground(0x000000, ptr0, fb_mapped0, screen_width, screen_height);
+        usleep(1);
+    }
+    Back_ground(0x000000, ptr0, fb_mapped0, screen_width, screen_height);
+    
+    end_display();
+    
+    return 0;
+}
+
 void Back_ground(int COLOR, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height)
 {
     int coor_y, coor_x;
@@ -289,48 +293,46 @@ void Back_ground(int COLOR, unsigned long *ptr, unsigned char *fb_mapped, int sc
     }
 }
 
-void Ball_DP(int X1, int Y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height)
+void Draw_Square(int X, int Y, int dX, int dY, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height)
 {
-    int size_x = 100;
-    int size_y = 100;
-
-    for(int x = X1; x < X1+size_x;x++)
+    for (int x = X; x < X + dX; x++)
     {
-        for(int y= Y1;y < Y1+size_y;y++)
+        for (int y = Y; y < Y + dY; y++)
         {
             ptr = (unsigned long *)fb_mapped + screen_width * y + x;
             *ptr = 0xFFFFFF;
         }
     }
-<<<<<<< HEAD
+}
 
-    for(int x = X1; x < X1+2*size_x;x++)
+void Ball_DP(int X1, int Y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height)
+{
+    int size_x = 100;
+    int size_y = 100;
+
+    for (int x = X1; x < X1 + size_x; x++)
     {
-        for(int y= Y1;y < Y1+2*size_y;y++)
+        for (int y = Y1; y < Y1 + size_y; y++)
         {
             ptr = (unsigned long *)fb_mapped + screen_width * y + x;
-            *ptr = 0xF000000F;
+            *ptr = 0xFFFFFF;
         }
     }
+}
 
-=======
->>>>>>> 2d88ff2d4cd16813b4d53602b80dc996a8abb616
-    // for (coor_y = 0; coor_y < Y1; coor_y++)
-    // {
-    //     ptr = (unsigned long *)fb_mapped + screen_width * coor_y;
-    //     for (coor_x = 0; coor_x < X1; coor_x++)
-    //     {
-    //         *ptr++ = 0x000000;
-    //     }
-    //     for (coor_x = X1; coor_x < X2; coor_x++)
-    //     {
-    //         *ptr++ = 0xFFFFFF;
-    //     }
-    //     for (coor_x = X2; coor_x < screen_width; coor_x++)
-    //     {
-    //         *ptr++ = 0x000000;
-    //     }
-    // }
+void Ball_RM(int X1, int Y1, unsigned long *ptr, unsigned char *fb_mapped, int screen_width, int screen_height)
+{
+    int size_x = 100;
+    int size_y = 100;
+
+    for (int x = X1; x < X1 + size_x; x++)
+    {
+        for (int y = Y1; y < Y1 + size_y; y++)
+        {
+            ptr = (unsigned long *)fb_mapped + screen_width * y + x;
+            *ptr = 0x000000;
+        }
+    }
 }
 
 /*
